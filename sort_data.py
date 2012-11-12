bc2d5be5b3ddf7f113c6892b434d016144bd85fd
Paul O'Neil <redballoon36@gmail.com>
2012-11-12 17:09:25 -0500

    Added other queries, path to output directory

--- sort_data.py
+++ sort_data.py
@@ -6,6 +6,7 @@ import csv
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input', type=str)
+parser.add_argument('--output', nargs='?', type=str, action='store', default='')
 
 args = parser.parse_args()
 
@@ -25,24 +26,48 @@ for (size, filename) in filenames.iteritems():
 
 
 # Write out the select a single cell times
-f = open('single_cell_select.csv', 'wb')
+f = open(os.path.join(args.output, 'single_cell_select.csv'), 'wb')
 csvout = csv.writer(f)
 for i in range(6):
 	csvout.writerow([ data[i][1], data[i][2], data[i][4], data[i][8] ] )
 
-
-f = open('cube_range_query.csv', 'wb')
+f = open(os.path.join(args.output, 'cube_range_query.csv'), 'wb')
 csvout = csv.writer(f)
 for i in range(6, 12):
 	csvout.writerow([ data[i][1], data[i][2], data[i][4], data[i][8] ] )
 
-f = open('slice_range_query.csv', 'wb')
+f = open(os.path.join(args.output, 'slice_range_query.csv'), 'wb')
 csvout = csv.writer(f)
 for i in range(12, 18):
 	csvout.writerow([ data[i][1], data[i][2], data[i][4], data[i][8] ] )
 
-f = open('equality.csv', 'wb')
+f = open(os.path.join(args.output, 'equality.csv'), 'wb')
 csvout = csv.writer(f)
 for i in range(18, 24):
+	csvout.writerow([ data[i][1], data[i][2], data[i][4], data[i][8] ] )
+
+f = open(os.path.join(args.output, 'window_2d.csv'), 'wb')
+csvout = csv.writer(f)
+for i in range(24, 28):
+	csvout.writerow([ data[i][1], data[i][2], data[i][4], data[i][8] ] )
+
+f = open(os.path.join(args.output, 'window_3d.csv'), 'wb')
+csvout = csv.writer(f)
+for i in range(28, 32):
+	csvout.writerow([ data[i][1], data[i][2], data[i][4], data[i][8] ] )
+
+f = open(os.path.join(args.output, 'window_5d.csv'), 'wb')
+csvout = csv.writer(f)
+for i in range(32, 36):
+	csvout.writerow([ data[i][1], data[i][2], data[i][4], data[i][8] ] )
+
+f = open(os.path.join(args.output, 'group_by_separate.csv'), 'wb')
+csvout = csv.writer(f)
+for i in [36+2x for x in range(6)]:
+	csvout.writerow([ data[i][1], data[i][2], data[i][4], data[i][8] ] )
+
+f = open(os.path.join(args.output, 'group_by_overlap.csv'), 'wb')
+csvout = csv.writer(f)
+for i in [37+2x for x in range(6)]:
 	csvout.writerow([ data[i][1], data[i][2], data[i][4], data[i][8] ] )
 
